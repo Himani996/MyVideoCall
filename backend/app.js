909a1a42ff94c5a3fdb2771a4db200c529813a33
Himani Gautam <himanigautam2109@gmail.com>
2026-01-30 15:36:56 +0530

    Refactor app.js to implement server and socket features
    Refactored app.js to enable server and socket functionality, added static file serving, and improved error handling.

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,4 @@
 
-// import express from "express";
-// import { createServer} from "node:http";
-// import {Server} from "socket.io";
-// import mongoose from "mongoose";
-// import { connectToSocket } from "./src/controllers/socketManager.js";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import userRoutes from "./src/routes/users.routes.js";
-
-
-// dotenv.config();
-
-// const app = express();
-// const server= createServer(app);
-// const io=connectToSocket(server);
-
-// app.set("port",(process.env.PORT||8000))
-// app.use(cors());
-// app.use(express.json({limit:"40kb"}));
-// app.use(express.urlencoded({limit:"40kb",extended:true}));
-
-// app.use("/api/v1/users",userRoutes);
-
-
-// app.get("/home", (req, res) => {
-//   res.json({ hello: "world" });
-// });
-
-// const start = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI);
-//     console.log("MongoDB Connected");
-
-//     const PORT = process.env.PORT || 8000;
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-
-//   } catch (error) {
-//     console.error("DB Connsection Failed ❌", error);
-//   }
-// };
-
-// start();
 
 
 import express from "express";
@@ -57,10 +13,8 @@ dotenv.config();
 
 const app = express();
 
-// 🔴 HTTP SERVER
 const server = createServer(app);
 
-// 🔴 SOCKET.IO ATTACH
 connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
@@ -81,7 +35,7 @@ const start = async () => {
 
     const PORT = process.env.PORT || 8000;
 
-    // ✅ IMPORTANT FIX
+
     server.listen(PORT, () => {
       console.log(`🚀 Server + Socket running on port ${PORT}`);
     });
@@ -90,5 +44,14 @@ const start = async () => {
     console.error("DB Connection Failed ❌", error);
   }
 };
+
+import path from "path";
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 
 start();
